c10a3bac028c09479e08ed75f1f97068ce37a350
František Maša <frantisekmasa1@gmail.com>
2017-09-04 01:02:49 +0200

    Fix indentation

--- src/DoctrineNullableEmbeddables/Subscriber.php
+++ src/DoctrineNullableEmbeddables/Subscriber.php
@@ -16,75 +16,75 @@ use Fmasa\DoctrineNullableEmbeddables\Annotations\Nullable;
 class Subscriber implements EventSubscriber
 {
 
-    /** @var Reader */
-    private $reader;
+	/** @var Reader */
+	private $reader;
 
 
-    public function __construct(Reader $reader)
-    {
-        $this->reader = $reader;
-    }
+	public function __construct(Reader $reader)
+	{
+		$this->reader = $reader;
+	}
 
 	public function getSubscribedEvents(): array
 	{
 		return ['postLoad'];
 	}
 
-    private function clearEmbeddablesIfNecessary($object, EntityManager $entityManager)
-    {
-        $metadata = $entityManager->getClassMetadata(get_class($object));
+	private function clearEmbeddablesIfNecessary($object, EntityManager $entityManager)
+	{
+		$metadata = $entityManager->getClassMetadata(get_class($object));
 
-        foreach($metadata->embeddedClasses as $fieldName => $embeddable) {
-        	if(strpos($fieldName, '.') !== false) {
-        		continue;
+		foreach ($metadata->embeddedClasses as $fieldName => $embeddable) {
+			if (strpos($fieldName, '.') !== false) {
+				continue;
 			}
 
 			$field = $metadata->getReflectionProperty($fieldName);
 			$value = $field->getValue($object);
 
-			if($value === null) {
+			if ($value === null) {
 				continue;
 			}
 
-        	if($this->hasNullableAnnotation($field)) {
+			if ($this->hasNullableAnnotation($field)) {
 				$this->clearEmbeddablesIfNecessary(
 					$value,
 					$entityManager
 				);
 
-				if($this->isEmpty($value, $entityManager->getClassMetadata($embeddable['class']))) {
+				if ($this->isEmpty($value, $entityManager->getClassMetadata($embeddable['class']))) {
 					$field->setValue($object, null);
 				}
 			}
 		}
-    }
+	}
 
-    public function postLoad(LifecycleEventArgs $args)
-    {
-    	$object = $args->getObject();
+	public function postLoad(LifecycleEventArgs $args)
+	{
+		$object = $args->getObject();
 
-    	if($object instanceof Proxy) {
-    		return;
+		if ($object instanceof Proxy) {
+			return;
 		}
 
-        $this->clearEmbeddablesIfNecessary(
-        	$object,
+		$this->clearEmbeddablesIfNecessary(
+			$object,
 			$args->getEntityManager()
 		);
-    }
+	}
 
-    private function isEmpty($object, ClassMetadata $metadata): bool
-    {
-    	foreach($metadata->getFieldNames() as $fieldName) {
-    		if($metadata->getFieldValue($object, $fieldName) !== null) {
-    			return false;
+	private function isEmpty($object, ClassMetadata $metadata): bool
+	{
+		foreach ($metadata->getFieldNames() as $fieldName) {
+			if ($metadata->getFieldValue($object, $fieldName) !== null) {
+				return false;
 			}
 		}
 
 		return true;
-    }
+	}
 
-    private function hasNullableAnnotation(\ReflectionProperty $property): bool
+	private function hasNullableAnnotation(\ReflectionProperty $property): bool
 	{
 		return $this->reader->getPropertyAnnotation($property, Nullable::class) !== null;
 	}
